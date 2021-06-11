30961d2f5970746896ca1410fea3f67245b4c810
AanandiMuthukumar <71145972+AanandiMuthukumar@users.noreply.github.com>
2021-06-11 22:44:46 +0530

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import App from './App';
-import {Home, About, Contact} from './pages/Index';
+import { useHistory } from 'react-router-dom';
+
 import { 
     BrowserRouter as Router, 
    Switch,
    Route,
 Link
 } from "react-router-dom";
-class Login extends React.Component {
-  
-   
-    
-    render() { 
-        return(
+class Login extends React.Component{
+    render(){
+        
+   //result = result.json
+  // localStorage.setItem(JSON.stringify(result))     
+   //history.pushState("/add")
+    return(
             <div class ='text-center p-10' className="form">
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
-                   <input type="text" name="username" placeholder="username" className="form-control"/><br />
+                   <input type="text" name="username" placeholder="username" onChange={(e)=>setUsername(e.target.value)} className="form-control"/><br />
                 </div>
                 <div className="form-group">
                        <label htmlFor="password">Password:</label>
-                       <input type="password" name="password" placeholder="password" className="form-control"/><br />
+                       <input type="password" name="password" placeholder="password" onChange={(e)=>setPassword(e.target.value)} className="form-control"/><br />
                 </div>
            <Router>
                <Switch>
@@ -30,7 +32,7 @@ class Login extends React.Component {
                   
                 <Link to ="/App">
                     <div className='footer'>
-                    <button type='button'>Login</button>
+                    <button onClick={login} type='button'>Login</button>
                 </div>  
                 </Link></Switch></Router>
             </div>
@@ -40,8 +42,27 @@ class Login extends React.Component {
 
             
         );
-
-    }
+        function Log(){
+            const[username,setUsername]=useState("");
+            const[password,setPassword]=useState("");
+           async function login(){
+               console.warn(username,password)
+               let item={username,password};
+               let result = fetch("http://testurl.com/login",{
+               method: 'POST',
+               headers: {
+                   "Content-Type":"application/json",
+                   "Accept":'application/json'
+               },
+               body: JSON.stringify(item)
+           });
+        }
+                }
+    
 }
- 
+}
+
+
 export default Login;
+    
+
