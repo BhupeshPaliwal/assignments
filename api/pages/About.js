d3c47b25fafe80ef1d27a7ddccbc6b6511efbb5e
AanandiMuthukumar <71145972+AanandiMuthukumar@users.noreply.github.com>
2021-06-11 22:46:04 +0530

    Create About.js

---
+++ api/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Layout} from '../layout';
+
+const About = () => <Layout>
+    <b><h1>About Page</h1></b>
+Name: AANANDI MUTHUKUMAR <br />
+Bits ID Number: 2019B3A30664H <br />
+Email: f20190664@hyderabad.bits-pilani.ac.in <br />
+Discipline: M.Sc.(Hons.) EconomicsB.E.(Hons.) Electrical & Electronics <br />
+  
+  </Layout>;
+export default About; 
