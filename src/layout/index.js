5845817023e80a00625042c29c7f29c3b514f752
AanandiMuthukumar <71145972+AanandiMuthukumar@users.noreply.github.com>
2021-06-11 16:54:42 +0530

    Update index.js

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+import Nav from './Nav';
+
+const Layout = (props) => (
+    <div class='text-center p-10'>
+        
+{props.children}
+    </div>
+);
+export { Layout, Nav };
