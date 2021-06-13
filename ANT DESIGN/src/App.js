87bbb5123f4304cb9e079f027136900b0bee5560
ksnandhan <85241079+ksnandhan@users.noreply.github.com>
2021-06-13 20:29:47 +0530

    Add files via upload

---
+++ ANT DESIGN/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./pages/home";
+//import "antd/dist/antd.css";
+
+const App = (props) => {
+  const Routes = Home ;
+  return <Routes />;
+};
+
+export default App;
