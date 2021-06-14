4e9f0632e507533827aa80c8941013fadaadf20b
Challa-Sai-Kumar <85490835+Challa-Sai-Kumar@users.noreply.github.com>
2021-06-14 11:38:50 +0530

    Add files via upload

---
+++ TIC-TAC-TOE/src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Layout} from '../Layout';
+
+const About = () => <Layout>
+    <b><h1>About Page</h1></b>
+Name: Sai Kumar Challa <br />
+College: BITS <br />
+Branch: EEE <br />
+
+  </Layout>;
+export default About; 
+
+
