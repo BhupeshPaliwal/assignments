d145e24da956ce792cb7f41d842aede45a76554e
unknown <shayan.majumder2@gmail.com>
2021-06-14 15:45:07 +0530

    init

---
+++ Q2,3Zip/Q2,3Zip/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Layout} from '../layout';
+
+const About = () => <Layout>
+    <b><h1>About Page</h1></b>
+Name: Pavan Anurag Malladi <br />
+Bits ID Number: 2019A3PS0404 <br />
+Email: f20190404@hyderabad.bits-pilani.ac.in <br />
+Discipline: B.E.(Hons.) Electrical & Electronics <br />
+  
+  </Layout>;
+export default About; 
