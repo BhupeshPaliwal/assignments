271a5f017adf04ef5b615148f69eeb30edffd226
AanandiMuthukumar <71145972+AanandiMuthukumar@users.noreply.github.com>
2021-07-20 23:55:26 +0530

    Create App.js

---
+++ proj2/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import ShowForm from './components/button';
+import Gauge  from "./components/gauge";
+
+
+function App() {
+  return (
+    <div>
+      <Gauge/>
+    </div>
+  );
+}
+
+export default App;
